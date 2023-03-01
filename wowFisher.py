291962fdc41aedf8de55493eab026fe889039226
mlamera <48600479+mlamera@users.noreply.github.com>
2023-03-01 10:50:24 -0800

    going sound route babyyy

--- wowFisher.py
+++ wowFisher.py
@@ -1,15 +1,49 @@
+from . import config
 import pyautogui
+import soundcard as sc
+import sys
+import numpy as np
 
-def screenCapture():
-    im1= pyautogui.screenshot('test.png')
-    location = pyautogui.locateCenterOnScreen('example.png', confidence=0.5)
-    print(location)
-    pyautogui.moveTo(location, duration=1)
-    if location != None:
-        pyautogui.press('=')
+
+
+def getSound():
+    speaker_id = sc.default_speaker().name if config.SPEAKER_ID == None else config.SPEAKER_ID
+    try:
+        # Get microphone using speaker ID and allow recording of speakers output.
+        with sc.get_microphone(id = speaker_id, include_loopback=True).recorder(samplerate = config.SAMPLE_RATE) as mic:
+            # Record audio
+            # numframes - number of frames to record (48000 * 1 = 48000 frames)
+            # data is a numpy array of recorded audio data
+            data = mic.record(numframes = config.SAMPLE_RATE * config.SEC)
+    # Lists available speakers if none found.
+    except IndexError:
+        print(f"Couldn't find speaker device '{speaker_id}'. Available options are:")
+        for speaker in sc.all_speakers():
+            print(speaker.name)
+        print("Set 'SPEAKER_ID' to one of the speakers listed above")
+        sys.exit(1)
+
+    # No idea what this means...
+    mean = sum(np.absolute(data)) / len(data)
+    mean = mean[0]
+    caught_fish = True if mean > config.SOUND_THRESHOLD else False
+    print(f"{i} catch = {caught_fish}: fish volume = {mean:9.5f}, speaker = '{speaker_id}'")
+
+    return caught_fish
+
+
+def wait():
+    return 0
 
 def main():
-    while True:
-        screenCapture()
+    """
+    1. Set timer
+    2. Check if WoW window selected
+    3. Start fishing
+    4. If sound catch fish and cast again, else ignore (timer between caught and cast ~0.5 secs)
+    5. Timer on casting If no sound by time recast press "-" (timer for recast)
+
+    """
+    return 0
 
 main()
